02ec11195654b4f38b0023cb898116b165a12af8
Power366 <172065612+Power366@users.noreply.github.com>
2026-05-18 13:54:55 -0500

    handle logout client side->clear memory,headers

--- frontend/src/componenets/Navbar.tsx
+++ frontend/src/componenets/Navbar.tsx
@@ -1,11 +1,12 @@
-import { useEffect,useState } from "react";
-import { Link } from "react-router"
+import { useDebugValue, useEffect,useState } from "react";
+import { Link,useNavigate } from "react-router"
 import { useAuth } from "../AuthContext"
 import type {User} from "./UserProfile";
 function Navbar(){
-    const {authFetch} = useAuth();
+    const {authFetch,setAccessToken} = useAuth();
     const [user, setUser] = useState<User>();
     const [error, setError] = useState(true);
+    const navigatge = useNavigate();
     useEffect(()=>{
          async function loadUser() {
             const response = await authFetch("http://localhost:3000/users/me", {
@@ -24,6 +25,23 @@ function Navbar(){
 
         void loadUser();
     },[authFetch])
+    async function handleLogout(){
+         const header = new Headers();
+            header.append("Content-Type","application/json");
+            header.set("authorization","");
+         const response = await authFetch("http://localhost:3000/logout", {
+                method: "GET",
+                headers:header
+            });
+            if (!response.ok) {
+                throw new Error("unsuccessfull logout");
+                
+            }
+            setAccessToken(null)
+            setError(true);
+            setUser(undefined);
+            navigatge("/");
+    }
     return(
         <div className="flex justify-between p-4">
         <div className="flex flex-3 justify-end ">
@@ -36,7 +54,7 @@ function Navbar(){
             <Link to="/signup"><h2>sign Up</h2></Link>
             <Link to="/login"><h2>login</h2></Link>
         </div> : <div className="links flex flex-2 gap-3 justify-end">
-            <Link to="/logout"><h2>log out</h2></Link>
+            <button className="hover:cursor-pointer" onClick={handleLogout}><h2>log out</h2></button>
              <Link to="/profile"><h2>{user?.email}</h2></Link>
         </div>}
 
